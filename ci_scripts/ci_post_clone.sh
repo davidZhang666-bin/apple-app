2be319225a6be14428f8582c9452c3904a432925
admin <m15097956329@163.com>
2026-08-15 15:21:22 +0800

    修改版本号1.0.1

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -7,4 +7,4 @@ echo "CocoaPods: $(pod --version)"
 script_dir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 cd "$script_dir/../KYHSApp"
 echo "Installing CocoaPods dependencies in $(pwd)"
-pod install --deployment --verbose
+pod install --verbose
